e5623b01a56755a0f899c6df10b717d81417007f
Crowdbotics <team@crowdbotics.com>
2023-07-07 14:19:21 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "getting-started-dem-41975",
+  url: "https://getting-started-dem-41975.botics.co",
+  api: "https://getting-started-dem-41975.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
